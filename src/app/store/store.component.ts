2ce906bfc00fbcf7d46934c284f909d88a0942ec
vkochetov-arhipov <vkochetov-arhipov@mmtr.ru>
2022-07-05 16:54:46 +0300

    chapter 7 p 135

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,19 +8,37 @@ import { Product } from '../model/product.model';
 })
 export class StoreComponent {
     public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
     constructor(
         private productRepository: ProductRepository) { }
 
     get products(): Product[] {
-        return this.productRepository.getProducts(this.selectedCategory);
+        const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.productRepository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
         return this.productRepository.getCategories();
     }
 
+    get pageCount(): number {
+        const selectedProductsQty = this.productRepository.getProducts(this.selectedCategory).length;
+        return Math.ceil(selectedProductsQty / this.productsPerPage);
+    }
+
     changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: string) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
     }
 }
