582ac0324b0ea13ce9348ba6d4b3befeec9a4bc4
vkochetov-arhipov <vkochetov-arhipov@mmtr.ru>
2022-07-06 14:51:43 +0300

    chapter 8 p 144

---
+++ src/app/store/cart-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Cart } from '../model/cart.model';
+
+@Component({
+    selector: 'app-cart-summary',
+    templateUrl: 'cart-summary.component.html'
+})
+export class CartSummaryComponent {
+    constructor(
+        public cart: Cart
+    ) { }
+}
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductRepository } from '../model/product.repository';
 import { Product } from '../model/product.model';
+import { Cart } from '../model/cart.model';
 
 @Component({
     selector: 'app-store',
@@ -12,7 +13,9 @@ export class StoreComponent {
     public selectedPage = 1;
 
     constructor(
-        private productRepository: ProductRepository) { }
+        private productRepository: ProductRepository,
+        private cart: Cart
+    ) { }
 
     get products(): Product[] {
         const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -40,5 +43,9 @@ export class StoreComponent {
     changePageSize(newSize: string) {
         this.productsPerPage = Number(newSize);
         this.changePage(1);
+    }
+
+    appProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
 }
