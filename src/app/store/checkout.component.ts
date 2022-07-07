24385144177c7ffb2d13df7cb5c526c87792eef6
vkochetov-arhipov <vkochetov-arhipov@mmtr.ru>
2022-07-07 12:26:09 +0300

    chapter 8 p 161

--- src/app/store/checkout.component.ts
+++ src/app/store/checkout.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { OrderRepository } from '../model/order.repository';
+import { Order } from '../model/order.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-checkout',
-    templateUrl: 'checkout.component.html'
+    templateUrl: 'checkout.component.html',
+    styleUrls: ['checkout.component.css']
 })
-export class CheckoutComponent {}
+export class CheckoutComponent {
+    orderSent = false;
+    submitted = false;
+
+    constructor(
+        public repository: OrderRepository,
+        public order: Order
+    ) { }
+
+    submitOrder(form: NgForm) {
+        this.submitted = true;
+        if (form.valid) {
+            this.repository.saveOrder(this.order).subscribe(order => {
+                this.order.clear();
+                this.orderSent = true;
+                this.submitted = false;
+            });
+        }
+    }
+
+}
