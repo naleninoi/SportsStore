ed4eb4994a34173024fc5ed3f7b29f3126994dd2
vkochetov-arhipov <vkochetov-arhipov@mmtr.ru>
2022-07-07 17:01:50 +0300

    chapter 9 p 169

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthComponent } from './auth.component';
+import { AdminComponent } from './admin.component';
+import { RouterModule } from '@angular/router';
+
+const routing = RouterModule.forChild([
+    { path: 'auth', component: AuthComponent },
+    { path: 'main', component: AdminComponent },
+    { path: '**', redirectTo: 'auth' },
+]);
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        routing
+    ],
+    declarations: [
+        AuthComponent,
+        AdminComponent
+    ]
+})
+export class AdminModule {}
