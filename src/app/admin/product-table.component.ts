11c1a5c630d943782dbe37c8b73daff0f8c73970
vkochetov-arhipov <vkochetov-arhipov@mmtr.ru>
2022-07-08 16:53:41 +0300

    chapter 9 p 186

--- src/app/admin/product-table.component.ts
+++ src/app/admin/product-table.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { ProductRepository } from '../model/product.repository';
+import { Product } from '../model/product.model';
 
 @Component({
     selector: 'app-product-table',
     templateUrl: 'product-table.component.html'
 })
 export class ProductTableComponent {
+    constructor(
+        private repository: ProductRepository
+    ) { }
+
+    getProducts(): Product[] {
+        return this.repository.getProducts();
+    }
+
+    deleteProduct(id: number): void {
+        this.repository.deleteProduct(id);
+    }
 
 }
